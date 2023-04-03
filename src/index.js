0c5fbfb450104a3ca02e6e6e28178e13dcf03e29
Natalia Blanco <nataliaouteiral@gmail.com>
2023-04-03 11:51:09 +0200

    add part of first endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const mysql = require('mysql2/promise');
 // create and config server
 const server = express();
 server.use(cors());
@@ -11,3 +11,46 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+server.get('/movies', function (req, res) {
+  res.json({
+    success: true,
+    movies: [
+      {
+        id: '1',
+        title: 'Gambita de dama',
+        genre: 'Drama',
+        image:
+          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg',
+      },
+      {
+        id: '2',
+        title: 'Friends',
+        genre: 'Comedia',
+        image:
+          '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg',
+      },
+    ],
+  });
+});
+
+mysql
+  .createConnection({
+    host: 'localhost',
+    database: 'Netflix',
+    user: 'root',
+    password: 'L0velace!',
+  })
+  .then((connection) => {
+    connection
+      .connect()
+      .then(() => {
+        console.log('Conectado con el identificador ' + connection.threadId);
+      })
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
